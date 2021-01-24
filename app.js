7c35c4648d1f903fee1a56e8f97999eb4bd8321a
jaihuni <kjhyama@gmail.com>
2021-01-25 01:48:13 +0900

    test delete

--- app.js
+++ app.js
@@ -4,21 +4,17 @@ const keys = require("./keys.json")
 
 const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setTitle('Jaihuni Logistics')
 	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
 	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Time', value: 'after et 05:00' },
+		{ name: 'volume limit', value: '50,000'},
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
 	)
-	.addField('Inline field title', 'Some value here', true)
+	
 	.setImage('https://i.imgur.com/wSTFkRM.png')
 	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
 
 
@@ -27,8 +23,10 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply(exampleEmbed);
+  if (msg.content === '!방수') {
+    sent = await msg.reply(exampleEmbed);
+    await asyncio.sleep(10);
+    await msg.delete_message(sent)
   }
 });
 
