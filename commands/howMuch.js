edfdc98bd68a7a2e3bcddd6074f9e8b46fab1d44
Guest <guest-rwinn7@h26831.sgb.skir.kr>
2021-09-21 23:37:37 +0900

    fix typo

--- commands/howMuch.js
+++ commands/howMuch.js
@@ -19,9 +19,9 @@ module.exports = {
 		var buymaxPrice;
 		var data;
 
-		await console.log(args.getInteger('int'));
+		await console.log(args.getInteger('itemid'));
 		await interaction.deferReply();
-		await fetch("https://api.evemarketer.com/ec/marketstat/json?typeid="+ args.getInteger('int') +"&regionlimit=1&usesystem=30000142")
+		await fetch("https://api.evemarketer.com/ec/marketstat/json?typeid="+ args.getInteger('itemid') +"&regionlimit=1&usesystem=30000142")
 		.then((response) => response.json())
 		.then((result) => {
 			data = result;
@@ -32,14 +32,14 @@ module.exports = {
 	
 			const embed = new MessageEmbed()
 				.setTitle('eveMarketer')
-				.setURL('https://evemarketer.com/types/'+args.getInteger('int'))
-				.setDescription("Item ID : "  + args.getInteger('int'))
+				.setURL('https://evemarketer.com/types/'+args.getInteger('itemid'))
+				.setDescription("Item ID : "  + args.getInteger('itemidt'))
 				.setThumbnail('https://i.imgur.com/AfFp7pu.png')
 				.addFields(
 					{ name: 'buy max', value: ''+buymaxPrice },
 					{ name: 'sell min', value: ''+sellminPrice }
 				)
-				.setThumbnail('https://imageserver.eveonline.com/Type/'+args.getInteger('int')+'_64.png')
+				.setThumbnail('https://imageserver.eveonline.com/Type/'+args.getInteger('itemid')+'_64.png')
 				.setColor("RANDOM")
 				.setTimestamp()
 				.setFooter(bot.user.username);
